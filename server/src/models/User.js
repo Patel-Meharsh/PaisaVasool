5c6adc4ca5816c26e7d3f8d877c78bfe9057c2f9
Meharsh Patel <meharsh.patel@thegatewaycorp.co.in>
2026-08-09 15:02:51 +0530

    Add User model

---
+++ server/src/models/User.js
@@ -0,0 +1,80 @@
+// Import mongoose
+const mongoose = require("mongoose");
+
+// Import bcryptjs for password hashing
+const bcrypt = require("bcryptjs");
+
+
+// Create the User schema
+const userSchema = new mongoose.Schema(
+    {
+        // User's full name
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        // User's email address
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        // User's password
+        password: {
+            type: String,
+            required: true,
+            minlength: 6
+        },
+
+        // User's role
+        // Normal users will be "customer"
+        // Admin users will be "admin"
+        role: {
+            type: String,
+            enum: ["customer", "admin"],
+            default: "customer"
+        }
+    },
+
+    // Automatically adds createdAt and updatedAt
+    {
+        timestamps: true
+    }
+);
+
+
+// Hash the password before saving the user
+userSchema.pre("save", async function (next) {
+
+    // If password hasn't been changed, don't hash it again
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    // Generate a salt
+    const salt = await bcrypt.genSalt(10);
+
+    // Hash the password
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+});
+
+
+// Compare a entered password with the stored hashed password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+// Create the User model
+const User = mongoose.model("User", userSchema);
+
+
+// Export the User model
+module.exports = User;
